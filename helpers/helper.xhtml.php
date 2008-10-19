d68e8f968f82b606aa5cee3f051f2382c8e7014a
Michael Pezzi <mike@thespiral.ca>
2008-10-19 17:47:55 -0400

    Added pagination display function

--- helpers/helper.xhtml.php
+++ helpers/helper.xhtml.php
@@ -38,6 +38,38 @@ function ul_tag( $items ) {
 	return ob_get_clean();
 }
 
+function pagination() {
+	global $config, $page, $paginate;
+	
+	ob_start();
+	
+	$pages = array();
+	
+	if ( $paginate['enabled'] ) {
+		$num_pages = ceil( $paginate['total'] / $paginate['per_page'] );
+		
+		if ( $paginate['page'] != 1 ) {
+			$pages[] = a_tag($page['url']."&page=". ($paginate['page'] - 1) ."&per_page=".$paginate['per_page'], "&laquo; Prev");
+		}
+		
+		for ($i=1;$i<=$num_pages;$i++) {
+			$pages[] = a_tag($page['url'] . "&page=" . $i . "&per_page=" . $paginate['per_page'], $i);
+		}
+		
+		if ( $paginate['page'] != $num_pages ) {
+			$pages[] = a_tag($page['url']."&page=". ($paginate['page'] + 1) ."&per_page=".$paginate['per_page'], "Next &raquo;");
+		}
+		
+		echo "<div class=\"pagination clear\">\n";
+		echo ul_tag($pages);
+		echo "</div>\n";
+		
+		echo "Pages: " . floor( $paginate['total'] / $paginate['per_page'] ) . "<br />\n";
+		echo "Total: " . $paginate['total'];
+	}
+	
+	return ob_get_clean();
+}
 
 
 
